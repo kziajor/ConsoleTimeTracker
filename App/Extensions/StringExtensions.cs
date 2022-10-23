e17117a1db3856faeaddb523aecc81666e1383bd
Krzysztof Ziajor <krzysztof.ziajor@gmail.com>
2022-10-23 19:37:31 +0200

    Basic string extensions

---
+++ App/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Extensions;
+
+public static class StringExtensions
+{
+   public static bool IsNullOrEmpty(this string @string)
+   {
+      return string.IsNullOrEmpty(@string);
+   }
+}
