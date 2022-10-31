19902c940835aac2422b1726a99cca736b01f73c
Krzysztof Ziajor <krzysztof.ziajor@gmail.com>
2022-10-31 09:37:08 +0100

    Common methods to get input from user

---
+++ App/Commands/CommandCommon.cs
@@ -0,0 +1,53 @@
+﻿using Spectre.Console;
+
+namespace App.Commands;
+
+public static class CommandCommon
+{
+   public static T AskFor<T>(string promptText)
+   {
+      var prompt = new TextPrompt<T>(promptText)
+                  .PromptStyle("green");
+
+      return AnsiConsole.Prompt(prompt);
+   }
+
+   public static T AskForWithEmptyAllowed<T>(string promptText)
+   {
+      var prompt = new TextPrompt<T>(promptText)
+                  .PromptStyle("green")
+                  .AllowEmpty();
+
+      return AnsiConsole.Prompt(prompt);
+   }
+
+   public static bool AskForYesNo(string promptText)
+   {
+      var choice = AnsiConsole.Prompt(
+               new SelectionPrompt<string>()
+                  .Title(promptText)
+                  .AddChoices(new[] { "Yes", "No" })
+            );
+
+      return choice == "Yes";
+   }
+
+   public static T ChooseOne<T>(this IEnumerable<T> choices, string promptText, int? pageSize = null, Func<T, string>? optionNameConverter = null) where T : class
+   {
+      var selectionPrompt = new SelectionPrompt<T>()
+         .Title(promptText)
+         .AddChoices(choices);
+
+      if (optionNameConverter is not null)
+      {
+         selectionPrompt.UseConverter(optionNameConverter);
+      }
+
+      if (pageSize is not null)
+      {
+         selectionPrompt.PageSize(pageSize.Value);
+      }
+
+      return AnsiConsole.Prompt(selectionPrompt);
+   }
+}
