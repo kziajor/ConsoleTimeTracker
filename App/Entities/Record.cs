0eb1d1da823385bb1f8c2c0b13fcd14f9b00aa43
Krzysztof Ziajor <krzysztof.ziajor@gmail.com>
2022-11-05 21:58:35 +0100

    [Records] Change column name StopedAt to FinishedAt in Records table
    Change Comment member in Record class to nullable

--- App/Entities/Record.cs
+++ App/Entities/Record.cs
@@ -4,9 +4,9 @@ public sealed class Record
 {
    public int? RE_Id { get; set; }
    public DateTime RE_StartedAt { get; set; }
-   public DateTime? RE_StopedAt { get; set; }
+   public DateTime? RE_FinishedAt { get; set; }
    public int RE_MinutesSpent { get; set; } = 0;
-   public string RE_Comment { get; set; } = string.Empty;
+   public string? RE_Comment { get; set; } = string.Empty;
    public int RE_RelTaskId { get; set; }
    public Task? Task { get; set; }
    public Project? Project { get; set; }
