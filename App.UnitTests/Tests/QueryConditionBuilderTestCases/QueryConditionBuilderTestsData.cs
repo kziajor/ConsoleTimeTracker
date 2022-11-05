b9a96c7be6cafd2144b577369f0c8af04fd53416
Krzysztof Ziajor <krzysztof.ziajor@gmail.com>
2022-11-05 15:51:48 +0100

    Query condition builder with unit tests

---
+++ App.UnitTests/Tests/QueryConditionBuilderTestCases/QueryConditionBuilderTestsData.cs
@@ -0,0 +1,101 @@
+﻿using App.Repositories.Helpers;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace App.UnitTests.Tests.QueryConditionBuilderTestCases;
+
+public class SingleSimpleConditionTestData : IEnumerable
+{
+   public IEnumerator GetEnumerator()
+   {
+      yield return new object[] { ConditionOperators.AND, "test = '1'", "test = '1'" };
+      yield return new object[] { ConditionOperators.OR, "test = '1'", "test = '1'" };
+      yield return new object[] { ConditionOperators.AND, "test LIKE '%1%'", "test LIKE '%1%'" };
+   }
+}
+
+public class MultipleSimpleConditionTestData : IEnumerable
+{
+   public IEnumerator GetEnumerator()
+   {
+      yield return new object[]
+      {
+         new List<QueryCondition>
+         {
+            new QueryCondition(ConditionOperators.AND, "test1 = '1'"),
+            new QueryCondition(ConditionOperators.AND, "test2 = '2'"),
+            new QueryCondition(ConditionOperators.AND, "test3 = '3'"),
+         },
+         "test1 = '1' AND test2 = '2' AND test3 = '3'"
+      };
+
+      yield return new object[]
+      {
+         new List<QueryCondition>
+         {
+            new QueryCondition(ConditionOperators.AND, "test1 = '1'"),
+            new QueryCondition(ConditionOperators.OR, "test2 = '2'"),
+            new QueryCondition(ConditionOperators.AND, "test3 = '3'"),
+         },
+         "test1 = '1' OR test2 = '2' AND test3 = '3'"
+      };
+   }
+}
+
+public class MultipleComplexConditionTestData : IEnumerable
+{
+   public IEnumerator GetEnumerator()
+   {
+      yield return new object[]
+      {
+         new List<QueryCondition>
+         {
+            new QueryCondition(ConditionOperators.AND, "test1 = '1'"),
+            new QueryCondition(ConditionOperators.AND, new QueryCondition[]
+            {
+               new QueryCondition(ConditionOperators.AND, "subtest1_1 = '1.1'"),
+               new QueryCondition(ConditionOperators.OR, "subtest1_2 = '1.2'"),
+               new QueryCondition(ConditionOperators.AND, "subtest1_3 = '1.3'"),
+            }),
+         },
+         "test1 = '1' AND (subtest1_1 = '1.1' OR subtest1_2 = '1.2' AND subtest1_3 = '1.3')"
+      };
+
+      yield return new object[]
+      {
+         new List<QueryCondition>
+         {
+            new QueryCondition(ConditionOperators.AND, "test1 = '1'"),
+            new QueryCondition(ConditionOperators.OR, new QueryCondition[]
+            {
+               new QueryCondition(ConditionOperators.AND, "subtest1_1 LIKE '%1.1'"),
+               new QueryCondition(ConditionOperators.AND, new QueryCondition[]
+               {
+                  new QueryCondition(ConditionOperators.AND, "subtest1_1_1 IS NOT NULL"),
+                  new QueryCondition(ConditionOperators.AND, "subtest1_1_2 IS NULL"),
+                  new QueryCondition(ConditionOperators.OR, "subtest1_1_3 IN ('1', '2', '3')"),
+               }),
+               new QueryCondition(ConditionOperators.AND, "subtest1_2 = '1.2'"),
+               new QueryCondition(ConditionOperators.AND, new QueryCondition[]
+               {
+                  new QueryCondition(ConditionOperators.AND, "subtest1_2_1 IS NOT NULL"),
+                  new QueryCondition(ConditionOperators.AND, "subtest1_2_2 IS NULL"),
+                  new QueryCondition(ConditionOperators.OR, "subtest1_2_3 IN ('1', '2', '3')"),
+               }),
+            }),
+            new QueryCondition(ConditionOperators.AND, "test2 = '2'"),
+            new QueryCondition(ConditionOperators.OR, new QueryCondition[]
+            {
+               new QueryCondition(ConditionOperators.AND, "subtest2_1 LIKE '%1.1'"),
+               new QueryCondition(ConditionOperators.AND, new QueryCondition[]
+               {
+                  new QueryCondition(ConditionOperators.AND, "subtest2_1_1 IS NOT NULL"),
+                  new QueryCondition(ConditionOperators.AND, "subtest2_1_2 IS NULL"),
+                  new QueryCondition(ConditionOperators.OR, "subtest2_1_3 IN ('1', '2', '3')"),
+               }),
+            }),
+         },
+         "test1 = '1' OR (subtest1_1 LIKE '%1.1' AND (subtest1_1_1 IS NOT NULL AND subtest1_1_2 IS NULL OR subtest1_1_3 IN ('1', '2', '3')) AND subtest1_2 = '1.2' AND (subtest1_2_1 IS NOT NULL AND subtest1_2_2 IS NULL OR subtest1_2_3 IN ('1', '2', '3'))) AND test2 = '2' OR (subtest2_1 LIKE '%1.1' AND (subtest2_1_1 IS NOT NULL AND subtest2_1_2 IS NULL OR subtest2_1_3 IN ('1', '2', '3')))"
+      };
+   }
+}
---
+++ App/Repositories/Helpers/QueryConditionBuilder.cs
@@ -0,0 +1,111 @@
+﻿using System.Text;
+
+namespace App.Repositories.Helpers;
+
+public class QueryConditionBuilder
+{
+   private readonly List<QueryCondition> _conditions = new();
+
+   public QueryConditionBuilder() { }
+
+   public QueryConditionBuilder(ConditionOperators conditionOperator, string condition)
+   {
+      _conditions.Add(new QueryCondition(conditionOperator, condition));
+   }
+
+   public QueryConditionBuilder Add(ConditionOperators conditionOperator, string condition)
+   {
+      _conditions.Add(new QueryCondition(conditionOperator, condition));
+      return this;
+   }
+
+   public QueryConditionBuilder Add(QueryCondition condition)
+   {
+      _conditions.Add(condition);
+      return this;
+   }
+
+   public QueryConditionBuilder(QueryCondition condition)
+   {
+      _conditions.Add(condition);
+   }
+
+   public QueryConditionBuilder AddRange(params QueryCondition[] conditions)
+   {
+      _conditions.AddRange(conditions);
+      return this;
+   }
+
+   public QueryConditionBuilder AddRange(ConditionOperators conditionOperator, params string[] conditions)
+   {
+      foreach (var simpleCondition in conditions)
+      {
+         _conditions.Add(new QueryCondition(conditionOperator, simpleCondition));
+      }
+      return this;
+   }
+
+   public override string ToString()
+   {
+      if (_conditions.Count == 0) { return string.Empty; }
+
+      var sb = new StringBuilder();
+
+      sb.Append(GetQueryConditionString(_conditions[0], true));
+
+      if (_conditions.Count == 1) { return sb.ToString(); }
+
+      foreach (var condition in _conditions.Skip(1))
+      {
+         sb.Append(' ').Append(GetQueryConditionString(condition));
+      }
+
+      return sb.ToString();
+   }
+
+   private static string GetQueryConditionString(QueryCondition condition, bool withoutOperator = false)
+   {
+      if (!string.IsNullOrEmpty(condition.Single))
+      {
+         return withoutOperator ? condition.Single : $"{condition.Operator} {condition.Single}";
+      }
+
+      var conditionGroup = condition.Group?.ToArray();
+
+      if (conditionGroup is null || conditionGroup.Length == 0) { return string.Empty; }
+
+      var queryConditionBuilder = new QueryConditionBuilder();
+      queryConditionBuilder.AddRange(conditionGroup);
+
+      var conditionGroupString = queryConditionBuilder.ToString();
+
+      if (string.IsNullOrEmpty(conditionGroupString)) { return string.Empty; }
+
+      return withoutOperator ? $"({conditionGroupString})" : $"{condition.Operator} ({conditionGroupString})";
+   }
+}
+
+public enum ConditionOperators
+{
+   AND,
+   OR,
+}
+
+public class QueryCondition
+{
+   public ConditionOperators Operator { get; }
+   public string? Single { get; }
+   public IEnumerable<QueryCondition>? Group { get; }
+
+   public QueryCondition(ConditionOperators conditionOperator, string condition)
+   {
+      Operator = conditionOperator;
+      Single = condition;
+   }
+
+   public QueryCondition(ConditionOperators conditionOperator, IEnumerable<QueryCondition> conditionGroup)
+   {
+      Operator = conditionOperator;
+      Group = conditionGroup;
+   }
+}
