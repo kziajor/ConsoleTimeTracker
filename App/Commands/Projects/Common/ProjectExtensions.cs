017367ccc609f74751b7d30799c371898274db8d
Krzysztof Ziajor <krzysztof.ziajor@gmail.com>
2022-11-06 23:14:39 +0100

    fix(projects): Escape markup in GetOptionLabel

--- App/Commands/Projects/Common/ProjectExtensions.cs
+++ App/Commands/Projects/Common/ProjectExtensions.cs
@@ -1,12 +1,13 @@
 ﻿using App.Entities;
+using Spectre.Console;
 
-namespace App.Commands.Projects;
+namespace App.Commands.Projects.Common;
 
 internal static class ProjectExtensions
 {
    public static string GetOptionLabel(this Project project)
    {
       var activityLabel = project.PR_Closed ? "not active" : "active";
-      return $"{project.PR_Id}\t{project.PR_Name} ({activityLabel})";
+      return $"{project.PR_Id}\t{project.PR_Name} ({activityLabel})".EscapeMarkup();
    }
 }
