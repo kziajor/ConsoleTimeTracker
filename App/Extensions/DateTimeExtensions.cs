35fae06495b08fd3ce8f59fe82911eaffde116c6
Krzysztof Ziajor <krzysztof.ziajor@gmail.com>
2022-11-06 23:18:35 +0100

    feature(extensions): Add DateTime extension ToIsoString

---
+++ App/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Extensions;
+
+public static class DateTimeExtensions
+{
+   public static string ToIsoString(this DateTime dateTime)
+   {
+      return dateTime.ToString("yyyy-MM-dd HH:mm");
+   }
+}
