4902215ebb0b53f70753de15c085aab93bbb9a26
Krzysztof Ziajor <krzysztof.ziajor@gmail.com>
2022-11-06 23:13:52 +0100

    feature: Add common commandline options

---
+++ App/Commands/CommandCommonOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.CommandLine;
+
+namespace App.Commands;
+
+public static class CommandCommonOptions
+{
+   public static Option<bool> InteractiveMode
+   {
+      get
+      {
+         var value = new Option<bool>(
+               name: "--interactive",
+               getDefaultValue: () => false,
+               description: "Use interactive mode to fill data"
+            );
+         value.AddAlias("-i");
+         return value;
+      }
+   }
+}
