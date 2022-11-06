2f7736b3d49d7c7a598e90c9c917e1c79a90a092
Krzysztof Ziajor <krzysztof.ziajor@gmail.com>
2022-11-06 23:17:48 +0100

    feature(commandline): Add extensions to get value from option or argument or default

---
+++ App/Extensions/CommandLineExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+
+namespace App.Extensions;
+
+public static class CommandLineExtensions
+{
+   public static T? GetValueForOptionOrDefault<T>(this ParseResult parseResult, Option<T>? option)
+   {
+      return option is not null ? parseResult.GetValueForOption(option) : default;
+   }
+
+   public static T? GetValueForArgumentOrDefault<T>(this ParseResult parseResult, Argument<T>? argument)
+   {
+      return argument is not null ? parseResult.GetValueForArgument(argument) : default;
+   }
+}
