2f3015ea80d1ebf14f6b616f1f40259e4b86fb2d
Krzysztof Ziajor <krzysztof.ziajor@gmail.com>
2022-11-06 23:17:04 +0100

    feature(AnsiConsole): Add WriteError and Grid.AddKeyValueRow extensions

--- App/Extensions/AnsiConsoleExtensions.cs
+++ App/Extensions/AnsiConsoleExtensions.cs
@@ -8,4 +8,39 @@ public static class AnsiConsoleExtensions
    {
       console.MarkupLineInterpolated($"[green]{key}[/]: {value}");
    }
+
+   public static void WriteError(this IAnsiConsole console, string errorMessage)
+   {
+      console.MarkupLineInterpolated($"[red]{errorMessage.EscapeMarkup()}[/]");
+   }
+
+   public static void WriteErrorAndExit(this IAnsiConsole console, string errorMessage, int errorCode = 0)
+   {
+      if (errorCode > 0)
+      {
+         errorMessage = $"[Error code: {errorCode}] " + errorMessage;
+      }
+      console.WriteError($"[red]{errorMessage.EscapeMarkup()}[/]");
+
+      Environment.Exit(errorCode);
+   }
+
+   public static Grid AddKeyValueRow<T>(this Grid grid, string key, T? value)
+   {
+      var valueString = value is not null
+         ? value switch
+         {
+            DateTime v => v.ToIsoString(),
+            _ => value?.ToString() ?? string.Empty,
+         }
+         : string.Empty;
+
+      grid.AddRow(new Text[]
+      {
+         new Text($"{key}:", new Style(Color.White)).RightAligned(),
+         new Text(valueString, new Style(Color.Green)).LeftAligned(),
+      });
+
+      return grid;
+   }
 }
