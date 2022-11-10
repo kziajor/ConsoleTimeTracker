2b9add517ad241f9383ee8f2192727e9d500a07b
Krzysztof Ziajor <krzysztof.ziajor@gmail.com>
2022-11-10 07:26:21 +0100

    Add app assets: Colors and Icons

---
+++ App/Assets/Colors.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Assets;
+
+public static class Colors
+{
+   public const string PRIMARY = "green";
+   public const string SECONDARY = "yellow";
+   public const string ERROR = "red";
+   public const string WARNING = "darkorange3";
+}
---
+++ App/Assets/Icons.cs
@@ -0,0 +1,11 @@
+﻿namespace App.Assets;
+
+public static class Icons
+{
+   public const string CLOCK_DEFAULT = ":five_o_clock:";
+   public const string CLOCK_PRIMARY = $"[{Colors.PRIMARY}]:five_o_clock:[/]";
+   public const string CLOCK_SECONDARY = $"[{Colors.SECONDARY}]:five_o_clock:[/]";
+   public const string CHECK_DEFAULT = ":check_mark_button:";
+   public const string CHECK_PRIMARY = $"[{Colors.PRIMARY}]:check_mark_button:[/]";
+   public const string CHECK_SECONDARY = $"[{Colors.SECONDARY}]:check_mark_button:[/]";
+}
