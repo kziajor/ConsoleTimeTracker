541d90653eac459e76b8969ac2fb20886a92176c
Krzysztof Ziajor <krzysztof.ziajor@gmail.com>
2022-11-13 12:01:34 +0100

    feature: Extension method to calculate hours from minutes represented by int

---
+++ App/Extensions/IntExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Extensions;
+
+public static class IntExtensions
+{
+   public static double MinutesToHours(this int minutes)
+   {
+      return Math.Round((double)minutes / 60, 2);
+   }
+}
