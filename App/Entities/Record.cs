a8a04a60e42c33dda5edc01c41c1efe1b7c5d3ea
Krzysztof Ziajor <krzysztof.ziajor@gmail.com>
2022-11-13 12:00:51 +0100

    refactor(Records): remove defaults in Record class

--- App/Entities/Record.cs
+++ App/Entities/Record.cs
@@ -1,12 +1,12 @@
 ﻿namespace App.Entities;
 
 public sealed class Record
-{
+{ // TODO: Create class that will inherite from Record class, will contain calculated fields: TimeSpentHours and will protect MinutesSpent value. Entities should be exacte mirror of tables in db
    public int? RE_Id { get; set; }
    public DateTime RE_StartedAt { get; set; }
    public DateTime? RE_FinishedAt { get; set; }
-   public int RE_MinutesSpent { get; set; } = 0;
-   public string? RE_Comment { get; set; } = string.Empty;
+   public int RE_MinutesSpent { get; set; }
+   public string? RE_Comment { get; set; }
    public int RE_RelTaskId { get; set; }
    public Task? Task { get; set; }
 
