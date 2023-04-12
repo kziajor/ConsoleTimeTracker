03820e442e082182a14f27fc55113565e976a67c
Krzysztof Ziajor <krzysztof.ziajor@gmail.com>
2023-04-12 10:18:06 +0200

    Reports - Month average working time - Add info about overtime hours

--- App/Commands/Reports/ReportMonthAverageTimePerDayCommand.cs
+++ App/Commands/Reports/ReportMonthAverageTimePerDayCommand.cs
@@ -11,42 +11,45 @@ namespace App.Commands.Reports;
 
 internal class ReportMonthAverageTimePerDayCommand : Command
 {
-   private readonly IAnsiConsole _console = ServicesProvider.GetInstance<IAnsiConsole>();
-   private readonly IDbRepository _dbRepository = ServicesProvider.GetInstance<IDbRepository>();
+    private readonly IAnsiConsole _console = ServicesProvider.GetInstance<IAnsiConsole>();
+    private readonly IDbRepository _dbRepository = ServicesProvider.GetInstance<IDbRepository>();
 
-   public ReportMonthAverageTimePerDayCommand() : base("month-average", "Average time spent on tasks per day in given month")
-   {
-      var monthOffset = ReportArguments.MonthOffset;
+    public ReportMonthAverageTimePerDayCommand() : base("month-average", "Average time spent on tasks per day in given month")
+    {
+        var monthOffset = ReportArguments.MonthOffset;
 
-      Add(monthOffset);
+        Add(monthOffset);
 
-      this.SetHandler((monthOffset) => ProjectSummaryHandler(monthOffset), monthOffset);
-   }
+        this.SetHandler((monthOffset) => ProjectSummaryHandler(monthOffset), monthOffset);
+    }
 
-   private void ProjectSummaryHandler(int monthOffset)
-   {
-      if (monthOffset > 0)
-      {
-         _console.WriteError("Couldn't calculate average time for future months");
-         return;
-      }
+    private void ProjectSummaryHandler(int monthOffset)
+    {
+        if (monthOffset > 0)
+        {
+            _console.WriteError("Couldn't calculate average time for future months");
+            return;
+        }
 
-      var monthStart = DateTime.Now.AddMonths(monthOffset).MonthBegin();
-      var monthEnd = monthStart.MonthEnd();
-      var records = _dbRepository.Records.GetByMonth(monthStart);
+        var monthStart = DateTime.Now.AddMonths(monthOffset).MonthBegin();
+        var monthEnd = monthStart.MonthEnd();
+        var records = _dbRepository.Records.GetByMonth(monthStart).Where(r => r.RE_StartedAt < DateTime.Now.DayBegin());
 
-      if (records is not null)
-      {
-         var workingDays = records.Select(r => r.RE_StartedAt.Date).Distinct().Count();
-         var totalHours = records.Sum(r => r.TimeSpentHours);
-         var average = workingDays > 0 ? totalHours / workingDays : totalHours;
+        if (records is not null)
+        {
+            var workingDays = records.Select(r => r.RE_StartedAt.Date).Distinct().Count();
+            var expectedHours = Convert.ToDecimal(workingDays * 7.5);
+            var totalHours = records.Sum(r => r.TimeSpentHours);
+            var overtimeHours = totalHours - expectedHours;
+            var average = workingDays > 0 ? totalHours / workingDays : totalHours;
 
-         _console.MarkupLineInterpolated($"Period of time: [{Colors.Primary}]{monthStart.ToIsoDate()} - {monthEnd.ToIsoDate()}[/]");
-         _console.MarkupLineInterpolated($"Average time per day: [{Colors.Primary}]{average:N2} hours[/]");
+            _console.MarkupLineInterpolated($"Period of time: [{Colors.Primary}]{monthStart.ToIsoDate()} - {monthEnd.ToIsoDate()}[/]");
+            _console.MarkupLineInterpolated($"Average time per day: [{Colors.Primary}]{average:N2} hours[/]");
+            _console.MarkupLineInterpolated($"Overtime hours: [{Colors.Primary}]{(overtimeHours >= 0 ? overtimeHours : ""):N2} hours[/]");
 
-         return;
-      }
+            return;
+        }
 
-      _console.MarkupLineInterpolated($"[{Colors.Warning}]Records not found in given period of time: {monthStart.ToIsoDate()} - {monthEnd.ToIsoDate()}[/]");
-   }
+        _console.MarkupLineInterpolated($"[{Colors.Warning}]Records not found in given period of time: {monthStart.ToIsoDate()} - {monthEnd.ToIsoDate()}[/]");
+    }
 }
